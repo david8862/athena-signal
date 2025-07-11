6239c201d40a66e45ab9d15a5e984a59f5016cc7
Xiaobin Zhang <david8862@gmail.com>
2025-07-11 17:33:38 +0800

    support stress test on board

--- testing/stress_test.sh
+++ testing/stress_test.sh
@@ -10,6 +10,23 @@
 # (open "cppcheck_report/index.html" with web brower to check)
 #
 #
+# install or build "bc" for stress test:
+# 1. for x86_64, just install with apt if needed:
+#    $ apt install bc
+#
+# 2. for ARM platform, use following steps to cross compile:
+#    git clone https://github.com/gavinhoward/bc.git
+#    cd bc
+#    export CC=<cross-compile-gcc path, like "/opt/ssu9383c_toolchain/aarch64-ssu9383c-linux-gnu/bin/aarch64-ssu9383c-linux-gnu-gcc">
+#    export HOSTCC=gcc
+#    export CFLAGS="-Wall -O2"
+#    ./configure.sh --bc-only --prefix=<install path, like "/mnt/e/bc_tool/aarch64-ssu9383c/">
+#    make
+#    make install
+#
+#    now the target platform bc will be under "--prefix" path
+#
+#
 # NOTE: This stress test script will be installed in and should be run at "build/install" dir!
 #
 if [[ "$#" -ne 1 ]]; then
@@ -21,6 +38,7 @@ LOOP_NUM=$1
 
 SCRIPT_PATH=$(dirname $(readlink -f "$0"))
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$SCRIPT_PATH/../lib/
+export PATH=$PATH:$SCRIPT_PATH/../testing/tools/
 
 pushd $SCRIPT_PATH/../
 mkdir output
@@ -28,12 +46,13 @@ mkdir output
 for((i=1;i<=$LOOP_NUM;i++));
 do
     rm -rf output/*
+    echo "Starting loop "$i
 
     # run MVDR beamforming test
     ./bin/samples/athena_signal_bf_sample -i testing/data/beamforming_1nan_3m_sound_0_noise_60_3channels.wav -b 1 -m 3 -f testing/data/beamforming_mic_coord.txt -l 90.0 -o output/beamforming_1nan_3m_sound_0_noise_60_mvdr_output.pcm
     # check if MVDR beamforming output match with the right result
     # due to potential calculation deviation, output may not strictly same as the right result
-    MVDR_RESULT_BYTES=$(du -b testing/data/beamforming_1nan_3m_sound_0_noise_60_mvdr_output.pcm | awk '{print $1}')
+    MVDR_RESULT_BYTES=$(ls -l testing/data/beamforming_1nan_3m_sound_0_noise_60_mvdr_output.pcm | awk '{print $5}')
     MVDR_DIFF_THRESHOLD=$(echo "$MVDR_RESULT_BYTES*0.01" | bc)
     MVDR_DIFF_BYTES=$(cmp -l testing/data/beamforming_1nan_3m_sound_0_noise_60_mvdr_output.pcm output/beamforming_1nan_3m_sound_0_noise_60_mvdr_output.pcm | wc -l | bc)
     MVDR_DIFF_RESULT=$(echo "$MVDR_DIFF_BYTES > $MVDR_DIFF_THRESHOLD" | bc)
@@ -46,8 +65,8 @@ do
     ./bin/samples/athena_signal_bf_sample -i testing/data/beamforming_1nan_3m_sound_0_noise_60_3channels.wav -b 2 -m 3 -f testing/data/beamforming_mic_coord.txt -l 90.0 -o output/beamforming_1nan_3m_sound_0_noise_60_gsc_output.pcm
     # check if GSC beamforming output match with the right result
     # due to potential calculation deviation, output may not strictly same as the right result
-    GSC_RESULT_BYTES=$(du -b testing/data/beamforming_1nan_3m_sound_0_noise_60_gsc_output.pcm | awk '{print $1}')
-    GSC_DIFF_THRESHOLD=$(echo "$GSC_RESULT_BYTES*0.06" | bc)
+    GSC_RESULT_BYTES=$(ls -l testing/data/beamforming_1nan_3m_sound_0_noise_60_gsc_output.pcm | awk '{print $5}')
+    GSC_DIFF_THRESHOLD=$(echo "$GSC_RESULT_BYTES*0.08" | bc)
     GSC_DIFF_BYTES=$(cmp -l testing/data/beamforming_1nan_3m_sound_0_noise_60_gsc_output.pcm output/beamforming_1nan_3m_sound_0_noise_60_gsc_output.pcm | wc -l | bc)
     GSC_DIFF_RESULT=$(echo "$GSC_DIFF_BYTES > $GSC_DIFF_THRESHOLD" | bc)
     if [ $GSC_DIFF_RESULT -eq 1 ]; then
@@ -59,7 +78,7 @@ do
     ./bin/samples/athena_signal_aec_sample -i testing/data/aec_input.wav -r testing/data/aec_ref.wav -o output/aec_output.pcm
     # check if AEC output match with the right result
     # due to potential calculation deviation, output may not strictly same as the right result
-    AEC_RESULT_BYTES=$(du -b testing/data/aec_output.pcm | awk '{print $1}')
+    AEC_RESULT_BYTES=$(ls -l testing/data/aec_output.pcm | awk '{print $5}')
     AEC_DIFF_THRESHOLD=$(echo "$AEC_RESULT_BYTES*0.08" | bc)
     AEC_DIFF_BYTES=$(cmp -l testing/data/aec_output.pcm output/aec_output.pcm | wc -l | bc)
     AEC_DIFF_RESULT=$(echo "$AEC_DIFF_BYTES > $AEC_DIFF_THRESHOLD" | bc)
@@ -72,7 +91,7 @@ do
     ./bin/samples/athena_signal_agc_sample -i testing/data/agc_input.wav -o output/agc_output.pcm
     # check if AGC output match with the right result
     # due to potential calculation deviation, output may not strictly same as the right result
-    AGC_RESULT_BYTES=$(du -b testing/data/agc_output.pcm | awk '{print $1}')
+    AGC_RESULT_BYTES=$(ls -l testing/data/agc_output.pcm | awk '{print $5}')
     AGC_DIFF_THRESHOLD=$(echo "$AGC_RESULT_BYTES*0.01" | bc)
     AGC_DIFF_BYTES=$(cmp -l testing/data/agc_output.pcm output/agc_output.pcm | wc -l | bc)
     AGC_DIFF_RESULT=$(echo "$AGC_DIFF_BYTES > $AGC_DIFF_THRESHOLD" | bc)
@@ -85,7 +104,7 @@ do
     ./bin/samples/athena_signal_hpf_sample -i testing/data/hpf_input.wav -o output/hpf_output.pcm
     # check if HPF output match with the right result
     # due to potential calculation deviation, output may not strictly same as the right result
-    HPF_RESULT_BYTES=$(du -b testing/data/hpf_output.pcm | awk '{print $1}')
+    HPF_RESULT_BYTES=$(ls -l testing/data/hpf_output.pcm | awk '{print $5}')
     HPF_DIFF_THRESHOLD=$(echo "$HPF_RESULT_BYTES*0.01" | bc)
     HPF_DIFF_BYTES=$(cmp -l testing/data/hpf_output.pcm output/hpf_output.pcm | wc -l | bc)
     HPF_DIFF_RESULT=$(echo "$HPF_DIFF_BYTES > $HPF_DIFF_THRESHOLD" | bc)
